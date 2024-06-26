4009dda97439d13b43e1df1b337ce78c7bf212b1
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-06-26 14:17:03 +0000

    Add blog routes

--- landing.js
+++ landing.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/landing.html');
-});
+router.get('/', (req, res) => res.sendFile(__dirname + '/public/landing.html'));
+router.get('/stats', (req, res) => res.sendFile(__dirname + '/public/stats.html'));
+
+router.get('/blog', (req, res) => res.render('blog', {req, res}));
+router.get('/blog/:id', (req, res) => res.render('blogpost', {req, res}));
 
 module.exports = router;
