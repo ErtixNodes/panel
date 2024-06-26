c04912788a3d1495aae085545448e480080efbd5
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-06-26 14:13:42 +0000

    Add logging

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { log } = console;
-
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev')); // log requests in dev format
+
 app.listen(3000, () => {
-    log(`App online at port 3000`);
+  log(`App online at port 3000`);
 });
