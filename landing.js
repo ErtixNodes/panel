0056ab71d2836bbb6e80615412b2e3032c5f6e42
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-08-11 14:55:57 +0000

    Move landing to ejs

--- landing.js
+++ landing.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.sendFile(__dirname + '/public/landing.html'));
+router.get('/', (req, res) => res.render('landing/index', {req, res}));
 router.get('/stats', (req, res) => res.sendFile(__dirname + '/public/stats.html'));
 
 router.get('/blog', (req, res) => res.render('blog', {req, res}));
