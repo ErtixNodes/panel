85afa74392f00073df71fc6fa8e99bde33b855b9
BasToTheMax <info@bastothemax.nl>
2024-10-04 09:20:49 +0200

    Finish home page

--- dash.js
+++ dash.js
@@ -86,6 +86,8 @@ app.use(async (req, res, next) => {
 });
 
 app.get('/', require('./dash/index.js'));
+app.get('/create', require('./dash/create.js'));
+
 app.get('/earn/cuty/time', require('./dash/time-cuty.js'));
 app.get('/earn/link/cuty', require('./dash/earn-cuty.js'));
 app.get('/earn/claim/:token', require('./dash/claim.js'));
